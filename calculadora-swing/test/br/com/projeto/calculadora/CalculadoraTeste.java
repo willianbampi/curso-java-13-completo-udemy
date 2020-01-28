910f4708ee56fa8f084b34663dc6b33c902bf22b
Willian Bampi <willian.bampi@gmail.com>
2020-01-28 19:58:57 -0300

    Add java class test.

---
+++ calculadora-swing/test/br/com/projeto/calculadora/CalculadoraTeste.java
@@ -0,0 +1,14 @@
+package br.com.projeto.calculadora;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CalculadoraTeste {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
